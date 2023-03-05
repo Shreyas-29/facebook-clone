9b65c7c90d5487433664c1fa87bae8c16778cbe4
29Shreyas <sihasaneshreyas@gmail.com>
2023-03-05 22:31:51 +0530

    Tenth

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,8 +3,15 @@ import Image from 'next/image';
 
 function Loading() {
     return (
-        <div className='flex items-center justify-center h-screen space-x-2'>
-            <Image src={'/loading.gif'} alt='' width={1000} height={1000} className='w-80 h-80' />
+        <div className='flex flex-col items-center justify-center h-screen space-x-2'>
+            <Image src={'/logo.png'} alt='' width={1000} height={1000} className='w-20 h-20 to-primary mb-10' />
+            <div className='flex flex-col items-center fixed bottom-10'>
+                <p className='text-slate-500 tracking-wider'>from</p>
+                <div className='flex items-center space-x-2'>
+                    <Image src={'/meta.png'} alt='' width={500} height={500} className='w-6 h-6 text-primary' />
+                    <p className='text-primary font-medium text-lg'>Meta</p>
+                </div>
+            </div>
         </div>
     )
 }
